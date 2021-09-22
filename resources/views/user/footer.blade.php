ac36e0273e0f28bbaa68767fd5d8d820fa6f0003
Arie Setiadi <ariesetiadi.edu@gmail.com>
2021-09-22 22:26:30 +0800

    Initial Commit

---
+++ resources/views/user/footer.blade.php
@@ -0,0 +1,53 @@
+ <!-- Bottom -->
+ <div id="bottom" class="clearfix has-spacer">
+     <div id="bottom-bar-inner" class="container">
+         <div class="bottom-bar-inner-wrap">
+             <div class="d-flex justify-content-center">
+                 <span class="text">Copyright © Desa Penglipuran 2021</span>
+             </div><!-- /.bottom-bar-content -->
+         </div><!-- /.bottom-bar-inner-wrap -->
+     </div><!-- /#bottom-bar-inner -->
+ </div><!-- /#bottom -->
+
+ </div><!-- /#page -->
+ </div><!-- /#wrapper -->
+
+ <a id="scroll-top"></a>
+
+ <!-- Javascript -->
+ <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+ <script src="{{ asset('assets/js/plugins.js') }}"></script>
+ {{-- <script src="{{ asset('assets/js/tether.min.js') }}"></script> --}}
+ <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+ <script src="{{ asset('assets/js/animsition.js') }}"></script>
+ <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+ {{-- <script src="{{ asset('assets/js/countto.js') }}"></script> --}}
+ {{-- <script src="{{ asset('assets/js/equalize.min.js') }}"></script> --}}
+ {{-- <script src="{{ asset('assets/js/jquery.isotope.min.js') }}"></script> --}}
+ <script src="{{ asset('assets/js/owl.carousel2.thumbs.js') }}"></script>
+
+ <script src="{{ asset('assets/js/jquery.cookie.js') }}"></script>
+ {{-- <script src="{{ asset('assets/js/gmap3.min.js') }}"></script> --}}
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIEU6OT3xqCksCetQeNLIPps6-AYrhq-s&region=GB"></script>
+ <script src="{{ asset('assets/js/shortcodes.js') }}"></script>
+ <script src="{{ asset('assets/js/main.js') }}"></script>
+
+ <!-- Revolution Slider -->
+ <script src="{{ asset('includes/rev-slider/js/jquery.themepunch.tools.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/jquery.themepunch.revolution.min.js') }}"></script>
+ <script src="{{ asset('assets/js/rev-slider.js') }}"></script>
+ <!-- Load Extensions only on Local File Systems ! The following part can be removed on Server for On Demand Loading -->
+ {{-- <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.actions.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.kenburn.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.migration.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.navigation.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+ <script src="{{ asset('includes/rev-slider/js/extensions/revolution.extension.video.min.js') }}"></script> --}}
+
+
+ </body>
+
+ </html>
---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,253 @@
+{{-- Import Header --}}
+@include('user.header')
+
+<!-- Main Content -->
+<div id="main-content" class="site-main clearfix">
+    <div id="content-wrap">
+        <div id="site-content" class="site-content clearfix">
+            <div id="inner-content" class="inner-content-wrap">
+                <div class="page-content">
+                    <!-- SLIDER -->
+                    <div class="rev_slider_wrapper fullwidthbanner-container">
+                        <div id="rev-slider1" class="rev_slider fullwidthbanner">
+                            <ul>
+                                <!-- Slide 1 -->
+                                <li data-transition="">
+                                    <!-- Main Image -->
+                                    <img src="{{ asset('assets/img/penglipuran/penglipuran1.jpg') }}" alt=""
+                                        data-bgposition="center center" data-no-retina>
+
+                                    <!-- Layers -->
+                                    <div class="tp-caption tp-resizeme text-white font-heading font-weight-600"
+                                        data-x="['left','left','left','center']" data-hoffset="['34','34','34','0']"
+                                        data-y="['middle','middle','middle','middle']"
+                                        data-voffset="['-134','-134','-134','-134']"
+                                        data-fontsize="['20','20','20','16']" data-lineheight="['70','70','40','24']"
+                                        data-width="full" data-height="none" data-whitespace="normal"
+                                        data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="700" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                        DESA WISATA PENGLIPURAN
+                                    </div>
+
+                                    <div class="tp-caption tp-resizeme text-white font-heading font-weight-700"
+                                        data-x="['left','left','left','center']" data-hoffset="['34','34','34','0']"
+                                        data-y="['middle','middle','middle','middle']"
+                                        data-voffset="['-63','-63','-63','-63']" data-fontsize="['52','52','42','32']"
+                                        data-lineheight="['65','65','45','35']" data-width="full" data-height="none"
+                                        data-whitespace="normal" data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="1000" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                        JAGALAH HATI
+                                    </div>
+
+                                    <div class="tp-caption tp-resizeme text-white font-heading font-weight-700"
+                                        data-x="['left','left','left','center']" data-hoffset="['34','34','34','0']"
+                                        data-y="['middle','middle','middle','middle']" data-voffset="['2','2','2','2']"
+                                        data-fontsize="['52','52','42','32']" data-lineheight="['65','65','45','35']"
+                                        data-width="full" data-height="none" data-whitespace="normal"
+                                        data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="1000" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                        BERSIHKAN PIKIRAN
+                                    </div>
+
+                                    <div class="tp-caption" data-x="['left','left','left','center']"
+                                        data-hoffset="['34','34','34','0']"
+                                        data-y="['middle','middle','middle','middle']"
+                                        data-voffset="['106','106','106','106']" data-width="full" data-height="none"
+                                        data-whitespace="normal" data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="1000" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                    </div>
+                                </li>
+                                <!-- /End Slide 1 -->
+
+                                <!-- Slide 2 -->
+                                <li data-transition="">
+                                    <!-- Main Image -->
+                                    <img src="{{ asset('assets/img/penglipuran/penglipuran2.jpg') }}" alt=""
+                                        data-bgposition="center center" data-no-retina>
+
+                                    <!-- Layers -->
+                                    <div class="tp-caption tp-resizeme text-white font-heading font-weight-600 text-center"
+                                        data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                                        data-y="['middle','middle','middle','middle']"
+                                        data-voffset="['-134','-134','-134','-134']"
+                                        data-fontsize="['20','20','20','16']" data-lineheight="['70','70','40','24']"
+                                        data-width="full" data-height="none" data-whitespace="normal"
+                                        data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="700" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                        A GLOBAL LEADER IN INFRASTRUCTURE
+                                    </div>
+
+                                    <div class="tp-caption tp-resizeme text-white font-heading font-weight-700 text-center"
+                                        data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                                        data-y="['middle','middle','middle','middle']"
+                                        data-voffset="['-63','-63','-63','-63']" data-fontsize="['52','52','42','32']"
+                                        data-lineheight="['65','65','45','35']" data-width="full" data-height="none"
+                                        data-whitespace="normal" data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="1000" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                        WE WILL BE HAPPY
+                                    </div>
+
+                                    <div class="tp-caption tp-resizeme text-white font-heading font-weight-700 text-center"
+                                        data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                                        data-y="['middle','middle','middle','middle']" data-voffset="['2','2','2','2']"
+                                        data-fontsize="['52','52','42','32']" data-lineheight="['65','65','45','35']"
+                                        data-width="full" data-height="none" data-whitespace="normal"
+                                        data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="1000" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                        TO TAKE CARE OF YOUR WORK
+                                    </div>
+
+                                    <div class="tp-caption text-center" data-x="['center','center','center','center']"
+                                        data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']"
+                                        data-voffset="['106','106','106','106']" data-width="full" data-height="none"
+                                        data-whitespace="normal" data-transform_idle="o:1;"
+                                        data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                        data-mask_in="x:0px;y:[100%];" data-mask_out="x:inherit;y:inherit;"
+                                        data-start="1000" data-splitin="none" data-splitout="none"
+                                        data-responsive_offset="on">
+                                    </div>
+                                </li>
+                                <!-- /End Slide 2 -->
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- END SLIDER -->
+
+                    <!-- ICONBOX -->
+                    <div class="row-iconbox">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="themesflat-spacer clearfix" data-desktop="60" data-mobile="60"
+                                        data-smobile="60"></div>
+                                    <div class="themesflat-headings style-1 text-center clearfix">
+                                        <h2 class="heading">YOUR BEST CHOOSE</h2>
+                                        <div class="sep has-icon width-125 clearfix">
+                                            <div class="sep-icon">
+                                                <span class="sep-icon-before sep-center sep-solid"></span>
+                                                <span class="icon-wrap"><i class="autora-icon-build"></i></span>
+                                                <span class="sep-icon-after sep-center sep-solid"></span>
+                                            </div>
+                                        </div>
+                                        <p class="sub-heading">More than 500 projects completed in Autora
+                                            -
+                                            With over 25 years of combined experience, we have the knowledge
+                                            to serve you.</p>
+                                    </div>
+                                    <div class="themesflat-spacer clearfix" data-desktop="42" data-mobile="35"
+                                        data-smobile="35"></div>
+                                </div><!-- /.col-md-12 -->
+                            </div><!-- /.row -->
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="themesflat-content-box clearfix" data-margin="0 5px 0 5px"
+                                        data-mobilemargin="0 0 0 0">
+                                        <div
+                                            class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
+                                            <div class="icon-wrap">
+                                                <i class="autora-icon-quality"></i>
+                                            </div>
+                                            <div class="text-wrap">
+                                                <h5 class="heading"><a href="#">BEST QUALITY</a></h5>
+                                                <div class="sep clearfix"></div>
+                                                <p class="sub-heading">Autora Construction Services are
+                                                    committed to meeting the highest quality standards
+                                                    without compromising our safety culture..</p>
+                                            </div>
+                                        </div><!-- /.themesflat-icon-box -->
+                                    </div><!-- /.themesflat-content-box -->
+                                </div><!-- /.col-md-4 -->
+                                <div class="col-md-4">
+                                    <div class="themesflat-spacer clearfix" data-desktop="0" data-mobile="0"
+                                        data-smobile="35"></div>
+                                    <div class="themesflat-content-box clearfix" data-margin="0 5px 0 5px"
+                                        data-mobilemargin="0 0 0 0">
+                                        <div
+                                            class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
+                                            <div class="icon-wrap">
+                                                <i class="autora-icon-time"></i>
+                                            </div>
+                                            <div class="text-wrap">
+                                                <h5 class="heading"><a href="#">ON TIME</a></h5>
+                                                <div class="sep clearfix"></div>
+                                                <p class="sub-heading">At our company, we respect the
+                                                    customer’s time and schedule and always complete the
+                                                    projects on timely fashion way.</p>
+                                            </div>
+                                        </div><!-- /.themesflat-icon-box -->
+                                    </div><!-- /.themesflat-content-box -->
+                                </div><!-- /.col-md-4 -->
+                                <div class="col-md-4">
+                                    <div class="themesflat-spacer clearfix" data-desktop="0" data-mobile="0"
+                                        data-smobile="35"></div>
+                                    <div class="themesflat-content-box clearfix" data-margin="0 5px 0 5px"
+                                        data-mobilemargin="0 0 0 0">
+                                        <div
+                                            class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
+                                            <div class="icon-wrap">
+                                                <i class="autora-icon-author"></i>
+                                            </div>
+                                            <div class="text-wrap">
+                                                <h5 class="heading"><a href="#">EXPERIENCED</a></h5>
+                                                <div class="sep clearfix"></div>
+                                                <p class="sub-heading">As the saying goes practice makes
+                                                    perfect. With our years of experience you can bet on us
+                                                    to get the job done exactly to your specifications.</p>
+                                            </div>
+                                        </div><!-- /.themesflat-icon-box -->
+                                    </div><!-- /.themesflat-content-box -->
+                                </div><!-- /.col-md-4 -->
+                            </div><!-- /.row -->
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="themesflat-spacer clearfix" data-desktop="41" data-mobile="35"
+                                        data-smobile="35"></div>
+                                    <div class="elm-button text-center">
+                                        <a href="#" class="themesflat-button bg-accent">ABOUT US</a>
+                                    </div>
+                                    <div class="themesflat-spacer clearfix" data-desktop="73" data-mobile="60"
+                                        data-smobile="60"></div>
+                                </div><!-- /.col-md-12 -->
+                            </div><!-- /.row -->
+                        </div><!-- /.container -->
+                    </div>
+                    <!-- END ICONBOX -->
+                </div><!-- /.page-content -->
+            </div><!-- /#inner-content -->
+        </div><!-- /#site-content -->
+    </div><!-- /#content-wrap -->
+</div><!-- /#main-content -->
+
+{{-- Import Footer --}}
+@include('user.footer')
