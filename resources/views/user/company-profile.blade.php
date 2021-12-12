80bfe2123a8c95d59a6cdba0b104eae517fd6726
Arie Setiadi <ariesetiadi.edu@gmail.com>
2021-12-12 18:06:57 +0800

    Menambah halaman company profile

--- resources/views/user/company-profile.blade.php
+++ resources/views/user/company-profile.blade.php
@@ -138,102 +138,23 @@
                             <div class="page-content">
 
                                 <div class="container">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="themesflat-spacer clearfix" data-desktop="60" data-mobile="60"
-                                                data-smobile="60"></div>
-                                            <div class="themesflat-headings style-1 text-center clearfix">
-                                                <h2 class="heading">COMPANY PROFILE</h2>
-                                                <div class="sep has-icon width-125 clearfix">
-                                                    <div class="sep-icon">
-                                                        <span class="sep-icon-before sep-center sep-solid"></span>
-                                                        <span class="icon-wrap"><i
-                                                                class="autora-icon-build"></i></span>
-                                                        <span class="sep-icon-after sep-center sep-solid"></span>
-                                                    </div>
-                                                </div>
-                                                <p class="sub-heading">More than 500 projects completed in Autora
-                                                    -
-                                                    With over 25 years of combined experience, we have the knowledge
-                                                    to serve you.</p>
-                                            </div>
-                                            <div class="themesflat-spacer clearfix" data-desktop="42" data-mobile="35"
-                                                data-smobile="35"></div>
-                                        </div><!-- /.col-md-12 -->
-                                    </div><!-- /.row -->
-
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="themesflat-content-box clearfix" data-margin="0 5px 0 5px"
-                                                data-mobilemargin="0 0 0 0">
-                                                <div
-                                                    class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
-                                                    <div class="icon-wrap">
-                                                        <i class="autora-icon-quality"></i>
-                                                    </div>
-                                                    <div class="text-wrap">
-                                                        <h5 class="heading"><a href="#">BEST QUALITY</a></h5>
-                                                        <div class="sep clearfix"></div>
-                                                        <p class="sub-heading">Autora Construction Services are
-                                                            committed to meeting the highest quality standards
-                                                            without compromising our safety culture..</p>
-                                                    </div>
-                                                </div><!-- /.themesflat-icon-box -->
-                                            </div><!-- /.themesflat-content-box -->
-                                        </div><!-- /.col-md-4 -->
-                                        <div class="col-md-4">
-                                            <div class="themesflat-spacer clearfix" data-desktop="0" data-mobile="0"
-                                                data-smobile="35"></div>
-                                            <div class="themesflat-content-box clearfix" data-margin="0 5px 0 5px"
-                                                data-mobilemargin="0 0 0 0">
-                                                <div
-                                                    class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
-                                                    <div class="icon-wrap">
-                                                        <i class="autora-icon-time"></i>
-                                                    </div>
-                                                    <div class="text-wrap">
-                                                        <h5 class="heading"><a href="#">ON TIME</a></h5>
-                                                        <div class="sep clearfix"></div>
-                                                        <p class="sub-heading">At our company, we respect the
-                                                            customer’s time and schedule and always complete the
-                                                            projects on timely fashion way.</p>
-                                                    </div>
-                                                </div><!-- /.themesflat-icon-box -->
-                                            </div><!-- /.themesflat-content-box -->
-                                        </div><!-- /.col-md-4 -->
-                                        <div class="col-md-4">
-                                            <div class="themesflat-spacer clearfix" data-desktop="0" data-mobile="0"
-                                                data-smobile="35"></div>
-                                            <div class="themesflat-content-box clearfix" data-margin="0 5px 0 5px"
-                                                data-mobilemargin="0 0 0 0">
-                                                <div
-                                                    class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
-                                                    <div class="icon-wrap">
-                                                        <i class="autora-icon-author"></i>
-                                                    </div>
-                                                    <div class="text-wrap">
-                                                        <h5 class="heading"><a href="#">EXPERIENCED</a></h5>
-                                                        <div class="sep clearfix"></div>
-                                                        <p class="sub-heading">As the saying goes practice makes
-                                                            perfect. With our years of experience you can bet on us
-                                                            to get the job done exactly to your specifications.</p>
-                                                    </div>
-                                                </div><!-- /.themesflat-icon-box -->
-                                            </div><!-- /.themesflat-content-box -->
-                                        </div><!-- /.col-md-4 -->
-                                    </div><!-- /.row -->
-
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="themesflat-spacer clearfix" data-desktop="41" data-mobile="35"
-                                                data-smobile="35"></div>
-                                            <div class="elm-button text-center">
-                                                <a href="#" class="themesflat-button bg-accent">ABOUT US</a>
-                                            </div>
-                                            <div class="themesflat-spacer clearfix" data-desktop="73" data-mobile="60"
-                                                data-smobile="60"></div>
-                                        </div><!-- /.col-md-12 -->
-                                    </div><!-- /.row -->
+                                    <h1 class="text-center my-5">COMPANY PROFILE</h1>
+                                    {{-- <center>
+                                        <div style="width: 80%; height: 600px;" class="rounded">
+                                            <video controls>
+                                                <source src="{{ asset('user/video/company-profile.mp4') }}"
+                                                    type="video/mp4">
+                                                Your browser does not support HTML video.
+                                            </video>
+                                        </div>
+                                    </center> --}}
+                                    {{-- <iframe src="https://www.youtube.com/watch?v=7YWOdPh0R9Q&ab_channel=MUSICOLABORASI"
+                                        width="100%" height="500px" allowfullscreen="true"
+                                        class="rounded"></iframe> --}}
+                                    <iframe width="100%" height="500px" src="https://www.youtube.com/embed/7YWOdPh0R9Q"
+                                        title="YouTube video player" frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                        allowfullscreen></iframe>
                                 </div><!-- /.container -->
                             </div><!-- /.page-content -->
                         </div><!-- /#inner-content -->
